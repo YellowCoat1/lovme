d2dddec7ba1fc3cd82a17a1bbc70817aec03e825
Jason Defillipo <jasond3663@gmail.com>
2023-09-08 21:10:30 -0800

    implemented encrypt/decrypt

--- server/crypto.lua
+++ server/crypto.lua
@@ -1,18 +1,32 @@
 local crypto = {}
 
--- local bitser = require 'bitser' -- serialization
+local bitser = require 'bitser' -- serialization
 -- local sock = require 'sock' -- networking
 local zen = require 'luazen' -- cryptography
 
 -- local user = require 'user' -- user class
 
--- generate keys 
-crypto.dh_sk = zen.randombytes(32)
-crypto.dh_pk = zen.x25519_public_key(crypto.dh_sk)
 
-function crypto:regen_keys()
-    self.dh_sk = zen.randombytes(32)
-    self.dh_pk = zen.x25519_public_key(crypto.dh_sk)
+crypto.emptyNonce = zen.b64decode("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
+
+-- key generation
+function crypto:gen_keys()
+    local dh_sk = zen.randombytes(32)
+    local dh_pk = zen.x25519_public_key(crypto.dh_sk)
+    return dh_sk, dh_pk
+end
+
+
+function crypto:encrypt(data, key)
+    data = bitser.dumps(data) -- serialize 
+    data = zen.encrypt(key, self.emptyNonce, data) -- encrypt
+    data = zen.lzma(data) -- compress
+    return data
+end
+
+-- TODO
+function crypto:decrypt(data, id, nonce)
+    data =
 end
 
 function crypto:shared_key(tPk)
