a5791697752538a6968d659badc7e1a6d57ffdcb
Jason Defillipo <jasond3663@gmail.com>
2023-09-09 20:32:44 -0800

    fixed crash

--- server/crypto.lua
+++ server/crypto.lua
@@ -15,19 +15,19 @@ end
 
 
 function crypto.encrypt(data, key)
-    local status, err = pcall( function()
+    local status, result = pcall( function()
         local emptyNonce = zen.b64decode("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
         data = bitser.dumps(data) -- serialize 
         data = zen.encrypt(key, emptyNonce, data) -- encrypt
         data = zen.lzma(data) -- compress
         return data
     end)
-    if not status then error("encryption failed: "..err, 2) end
-    return status
+    if status == false then error("encryption failed: " .. result, 2) end
+    return result
 end
 
 function crypto.decrypt(data, key)
-    local status, err = pcall( function()
+    local status, result = pcall( function()
         local emptyNonce = zen.b64decode("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
         data = zen.unlzma(data)
         data = zen.decrypt(key, emptyNonce, data)
@@ -35,8 +35,8 @@ function crypto.decrypt(data, key)
         return data
     end)
 
-    if not status then error("decryption failed: "..err, 2) end
-    return status
+    if status == false then error("decryption failed: "..result, 2) end
+    return result
 end
 
 function crypto.shared_key(oSk, tPk)
