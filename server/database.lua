b85cc1ff7ca7edcb05249c49126cc8673d2cdd58
Jason Defillipo <jasond3663@gmail.com>
2023-09-10 00:18:57 -0800

    added test_username to database module

--- server/database.lua
+++ server/database.lua
@@ -7,13 +7,22 @@ local zen = require 'luazen' -- cryptography
 love.filesystem.setIdentity("LOVME_server")
 
 
-local user_dir_info = love.filesystem.getInfo("users")
-if not user_dir_info then
+local users_dir_info = love.filesystem.getInfo("users")
+if not users_dir_info then
     love.filesystem.createDirectory("users")
-elseif not user_dir_info.type == "directory" then
+elseif not users_dir_info.type == "directory" then
     error("users file found; not directory.")
 end
 
+local function test_username(username)
+    assert(type(username) == "string", "invalid username in test_username")
+    -- banned characters: spaces, [, ], (, ), :, ;, %
+    if username:find("[%s%[%]%(%)%.%:%;%%]") then
+        return false
+    end
+    return true
+        
+end
 
 
 
