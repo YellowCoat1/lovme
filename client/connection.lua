280eb8c690a164d24f90c25d25cf1ae5d538cf60
Jason Defillipo <jasond3663@gmail.com>
2023-09-12 16:55:56 -0800

    isolated client-side server messaging code

--- client/connection.lua
+++ client/connection.lua
@@ -14,6 +14,18 @@ local database_public_keys = {}
 sock_client:connect()
 
 
+local function sendToServer(message, sendData)
+    if not sock_client then print("sock_client not found") return false end
+    local sesh_id = sock_client.test_id
+    if not sesh_id then print("SID not found") return false end
+    local sendTable = {}
+    sendTable.SID = sesh_id
+    sendTable.data = {}
+    _, sendTable.data = crypto.encrypt(sendData, sock_client.shared_key)
+    sock_client:send(message, sendData)
+end
+
+
 sock_client:on("connect", function()
     local sendTable = {}
     sendTable.upk = client_public_key
@@ -25,19 +37,19 @@ sock_client:on("key_response", function(data)
     sock_client.test_id = data.sessionID
     sock_client.shared_key = zen.key_exchange(client_secret_key, data.spk)
 
-    -- login
-    local sendTable = {}
-    sendTable.SID = sock_client.test_id
-    sendTable.data = {}
-    sendTable.data.user = "user1"
-    sendTable.data.pass = "password"
-
+    -- gen database keys
     local bytes16Salt = zen.b64decode("ABCDEFGHIJKLMNOPQRSTUV")
     sock_client.database_secret = zen.argon2i(sendTable.data.pass, bytes16Salt, 200, 15)
     sock_client.database_public = zen.x25519_public_key(sock_client.database_secret)
 
-    _, sendTable.data = crypto.encrypt(sendTable.data, sock_client.shared_key)
-    sock_client:send("login", sendTable)
+    -- login
+    local sendTable = {}
+    sendTable.user = "user1"
+    sendTable.pass = "password"
+
+
+    local status = sendToServer("login", sendTable)
+    if not status then print("server send failed") end
 end)
 
 sock_client:on("login-success", function(data)
@@ -67,9 +79,9 @@ end)
 
 -- for debugging
 sock_client:on("usr_error", function(data)
-    local status, data = crypto.decrypt(data, sock_client.shared_key)
-    if not data then return end
-    print(data[1])
+    local status, result = crypto.decrypt(data, sock_client.shared_key)
+    if not result then return end
+    print(result[1])
 end)
 
 function connection.update()
